f6eeb0e2b4ca0780d81f7677fb5e73a85a05b6a9
Yuichi Araki <yaraki@google.com>
2023-01-11 10:25:57 +0900

    Set up spotless
    Change-Id: Ib0c557d5d3d0ee0f4e797d77c60311963a48c595

--- app/src/androidTest/java/com/example/android/social/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/android/social/ExampleInstrumentedTest.kt
@@ -18,7 +18,7 @@ package com.example.android.social
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 
