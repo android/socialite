f9257c9f8dcb41ce7971df57d3ad940b058a49ea
yongsuk44 <rlarladyd@gmail.com>
2024-03-21 09:07:18 +0900

    Modify 'Button + Text' component to 'TextButton'

--- app/src/main/java/com/google/android/samples/socialite/ui/home/Settings.kt
+++ app/src/main/java/com/google/android/samples/socialite/ui/home/Settings.kt
@@ -16,49 +16,44 @@
 
 package com.google.android.samples.socialite.ui.home
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.google.android.samples.socialite.R
 
 @Composable
 fun Settings(
     contentPadding: PaddingValues,
     modifier: Modifier = Modifier,
+    viewModel: SettingsViewModel = hiltViewModel(),
 ) {
-    val viewModel: SettingsViewModel = hiltViewModel()
-    LazyColumn(
-        modifier = modifier,
-        contentPadding = contentPadding,
+    Column(
+        modifier = modifier
+            .padding(contentPadding)
+            .padding(32.dp),
     ) {
-        item {
-            Box(modifier = Modifier.padding(32.dp)) {
-                Button(
-                    onClick = { viewModel.clearMessages() },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .heightIn(min = 56.dp),
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = MaterialTheme.colorScheme.primaryContainer,
-                        contentColor = MaterialTheme.colorScheme.onPrimaryContainer,
-                    ),
-                ) {
-                    Text(text = stringResource(R.string.clear_message_history))
-                }
-            }
+        TextButton(
+            onClick = { viewModel.clearMessages() },
+            modifier = Modifier
+                .fillMaxWidth()
+                .heightIn(min = 56.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                contentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+            ),
+        ) {
+            Text(text = stringResource(R.string.clear_message_history))
         }
     }
 }
