abd564e0743df66ebe1d03efaacccd837916c9c1
Ash <83780687+ashnohe@users.noreply.github.com>
2024-03-23 14:08:57 -0700

    Add Navigation Rail for large screen devices (#37)
    Add Navigation Rail for large screen devices using m3 adaptive layout.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,6 +88,7 @@ dependencies {
     implementation(libs.compose.ui.text.google.fonts)
     implementation(libs.compose.ui.tooling.preview)
     implementation(libs.compose.material3)
+    implementation(libs.compose.material3.adaptive)
     implementation(libs.compose.material.icons)
     androidTestImplementation(libs.compose.ui.test)
     debugImplementation(libs.compose.ui.tooling)
--- app/src/main/java/com/google/android/samples/socialite/ui/home/Home.kt
+++ app/src/main/java/com/google/android/samples/socialite/ui/home/Home.kt
@@ -24,12 +24,12 @@ import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material.icons.outlined.VideoLibrary
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.adaptive.navigationsuite.ExperimentalMaterial3AdaptiveNavigationSuiteApi
+import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -38,10 +38,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -49,27 +49,51 @@ import com.google.android.samples.socialite.R
 import com.google.android.samples.socialite.ui.AnimationConstants
 import com.google.android.samples.socialite.ui.home.timeline.Timeline
 
+@OptIn(ExperimentalMaterial3AdaptiveNavigationSuiteApi::class)
 @Composable
 fun Home(
     onChatClicked: (chatId: Long) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    var destination by rememberSaveable { mutableStateOf(Destination.Chats) }
+    var currentDestination by rememberSaveable { mutableStateOf(Destination.Chats) }
+    NavigationSuiteScaffold(
+        navigationSuiteItems = {
+            for (destination in Destination.entries) {
+                val selected = currentDestination.route == destination.route
+                item(
+                    selected = selected,
+                    onClick = { currentDestination = destination },
+                    icon = {
+                        Icon(
+                            imageVector = destination.imageVector,
+                            contentDescription = stringResource(destination.label),
+                        )
+                    },
+                    label = {
+                        Text(text = stringResource(destination.label))
+                    },
+                    alwaysShowLabel = false,
+                )
+            }
+        },
+    ) { HomeContent(currentDestination, modifier, onChatClicked) }
+}
+
+@Composable
+private fun HomeContent(
+    currentDestination: Destination,
+    modifier: Modifier,
+    onChatClicked: (chatId: Long) -> Unit,
+) {
     Scaffold(
         modifier = modifier,
-        topBar = { HomeAppBar(title = stringResource(destination.label)) },
-        bottomBar = {
-            HomeNavigationBar(
-                currentDestination = destination.route,
-                onDestinationChanged = { destination = it },
-            )
-        },
+        topBar = { HomeAppBar(title = stringResource(currentDestination.label)) },
     ) { innerPadding ->
         val navController = rememberNavController()
         HomeBackground(modifier = Modifier.fillMaxSize())
         NavHost(
             navController = navController,
-            startDestination = destination.route,
+            startDestination = currentDestination.route,
             modifier = modifier,
         ) {
             composable(
@@ -145,34 +169,4 @@ private enum class Destination(
         label = R.string.settings,
         imageVector = Icons.Outlined.Settings,
     ),
-}
-
-@Composable
-private fun HomeNavigationBar(
-    currentDestination: String,
-    onDestinationChanged: (Destination) -> Unit,
-    modifier: Modifier = Modifier,
-) {
-    NavigationBar(
-        modifier = modifier,
-    ) {
-        for (destination in Destination.values()) {
-            val selected = currentDestination == destination.route
-            val label = stringResource(destination.label)
-            NavigationBarItem(
-                selected = selected,
-                onClick = { onDestinationChanged(destination) },
-                icon = {
-                    Icon(
-                        imageVector = destination.imageVector,
-                        contentDescription = label,
-                    )
-                },
-                label = {
-                    Text(text = label)
-                },
-                alwaysShowLabel = false,
-            )
-        }
-    }
 }
