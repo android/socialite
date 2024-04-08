dea1ac9b1f44e5645f25659c13ca4213b09faf71
Francesco Romano <romanofranz@google.com>
2024-04-08 11:54:22 +0200

    Add WindowManager missing dependency

--- app/src/main/java/com/google/android/samples/socialite/MainActivity.kt
+++ app/src/main/java/com/google/android/samples/socialite/MainActivity.kt
@@ -18,6 +18,7 @@ package com.google.android.samples.socialite
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
