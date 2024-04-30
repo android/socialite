b050a559be9f345640b3c104081ac2c2b526398f
Summers Pittman <secondsun@gmail.com>
2024-04-30 13:18:51 -0400

    Minor Fixups for Codelab
    Change-Id: I42a5d795e85308351cc86089ef5f4c77ab8b8091

--- app/src/main/java/com/google/android/samples/socialite/MainActivity.kt
+++ app/src/main/java/com/google/android/samples/socialite/MainActivity.kt
@@ -20,7 +20,6 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.core.content.pm.ShortcutManagerCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.google.android.samples.socialite.ui.Main
