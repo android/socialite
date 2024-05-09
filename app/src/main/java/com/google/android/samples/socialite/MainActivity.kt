9de502f2e29b551112f8e4b64da5cfdc8688ab38
Ash Nohe <ashnohe@google.com>
2024-05-09 11:28:34 -0700

    add back in edge to edge

--- app/src/main/java/com/google/android/samples/socialite/MainActivity.kt
+++ app/src/main/java/com/google/android/samples/socialite/MainActivity.kt
@@ -17,9 +17,11 @@
 package com.google.android.samples.socialite
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.core.content.pm.ShortcutManagerCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.glance.appwidget.updateAll
@@ -33,6 +35,9 @@ import kotlinx.coroutines.runBlocking
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
+        enableEdgeToEdge(if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            window.isNavigationBarContrastEnforced = false
+        }
         super.onCreate(savedInstanceState)
         runBlocking { SociaLiteAppWidget().updateAll(this@MainActivity) }
         setContent {
