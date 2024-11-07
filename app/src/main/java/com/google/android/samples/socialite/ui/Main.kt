ee585feaa6d053575d177380798e2c8e42c0cc6a
Tram Bui <trambui@google.com>
2024-11-07 14:27:50 -0800

    add in new animations for pb NavHost and update the activity and nav dependencies

--- app/src/main/java/com/google/android/samples/socialite/ui/Main.kt
+++ app/src/main/java/com/google/android/samples/socialite/ui/Main.kt
@@ -20,6 +20,7 @@ import android.app.Activity
 import android.content.Intent
 import android.content.pm.ActivityInfo
 import android.os.Bundle
+import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.core.FastOutLinearInEasing
 import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.tween
@@ -30,6 +31,7 @@ import androidx.compose.animation.scaleOut
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination
@@ -82,11 +84,14 @@ fun MainNavigation(
     NavHost(
         navController = navController,
         startDestination = "home",
-        popEnterTransition = {
-            scaleIn(initialScale = 1.1F) + fadeIn()
-        },
         popExitTransition = {
-            scaleOut(targetScale = 0.9F) + fadeOut()
+            scaleOut(
+                targetScale = 0.9f,
+                transformOrigin = TransformOrigin(pivotFractionX = 0.5f, pivotFractionY = 0.5f),
+            )
+        },
+        popEnterTransition = {
+            EnterTransition.None
         },
         modifier = modifier,
     ) {
