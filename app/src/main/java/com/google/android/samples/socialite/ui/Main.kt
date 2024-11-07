1b84375e03a1ea48ac92530845844a89043e0548
Tram Bui <trambui@google.com>
2024-11-07 14:44:29 -0800

    fix spotlessApply error

--- app/src/main/java/com/google/android/samples/socialite/ui/Main.kt
+++ app/src/main/java/com/google/android/samples/socialite/ui/Main.kt
@@ -26,7 +26,6 @@ import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
-import androidx.compose.animation.scaleIn
 import androidx.compose.animation.scaleOut
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
