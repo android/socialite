1b4b75821e3d10f926d67d98344b086a4f6dd871
Ash Nohe <ashnohe@google.com>
2024-12-09 12:50:37 -0800

    update to target SDK 35 and dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,12 +34,12 @@ secrets {
 
 android {
     namespace = "com.google.android.samples.socialite"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.google.android.samples.socialite"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
